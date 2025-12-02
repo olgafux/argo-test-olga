892be087f694b14cfc9764ce10bc5fd07d275d3c
olgafux <olga.fux@gmail.com>
2025-12-02 12:45:31 +0200

    Update app.js

--- argo-global-olga/src/app.js
+++ argo-global-olga/src/app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Olga 4!\n');
+  res.end('Hello, OlgaF 4!\n');
 });
 
 server.listen(port, hostname, () => {
